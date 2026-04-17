00a1ebfd1b9e840d06bc746b0b4c58e6ee18f0b0
alejobernuysergio-star <alejobernuysergio@gmail.com>
2026-04-17 11:08:29 -0500

    Delete ejercicios_RUTA_1_SERGIO_ALEJO/ejercicios ruta 1 directory

--- ejercicios_RUTA_1_SERGIO_ALEJO/ejercicios ruta 1/ejercicio_5_practico_CODELAB_7.kt
+++
@@ -1,9 +0,0 @@
-//concatenar ints con " ", no van a sumarse en un nuevo parametro
-//se guardan como string por las comillas
-//se muestran como string al llamado println
-fun main() {
-    val baseSalary = 5000
-    val bonusAmount = 1000
-    val totalSalary = "$baseSalary + $bonusAmount"
-    println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
-}
